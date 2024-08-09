8a5655b96d4a1fefdb5c981021e0abc7a7cced94
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2024-08-09 10:11:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 // } Driver Code Ends
 
-
 class Solution {
 public:
     int mod = 1e9+7;
@@ -12,34 +11,29 @@ public:
     int countSubsetSum(vector<int>& arr, int sum, int n, vector<vector<int>>& dp) {
         // Base cases
         if (n == 0) {
-            if (sum == 0 && arr[0] == 0) {
-                return 2;
-            }
-            if (sum == 0 || sum == arr[0]) {
+            if (sum == 0) {
                 return 1;
             }
             return 0;
         }
 
-       
-        if (dp[n][sum] != -1) {
-            return dp[n][sum];
+        if (dp[n-1][sum] != -1) {
+            return dp[n-1][sum];
         }
 
         int notTake = countSubsetSum(arr, sum, n-1, dp) % mod;
 
         int take = 0;
-        if (arr[n] <= sum) {
-            take = countSubsetSum(arr, sum-arr[n], n-1, dp) % mod;
+        if (arr[n-1] <= sum) {
+            take = countSubsetSum(arr, sum-arr[n-1], n-1, dp) % mod;
         }
 
-        return dp[n][sum] = (take + notTake) % mod;
+        return dp[n-1][sum] = (take + notTake) % mod;
     }
 
     int countPartitions(int n, int d, vector<int>& arr) {
         int sum = accumulate(arr.begin(), arr.end(), 0);
 
-       
         if ((sum - d) % 2 != 0 || (sum - d) < 0) {
             return 0;
         }
@@ -47,7 +41,7 @@ public:
         int subsetSum = (sum - d) / 2;
 
         vector<vector<int>> dp(n, vector<int>(subsetSum + 1, -1));
-        return countSubsetSum(arr, subsetSum, n-1, dp);
+        return countSubsetSum(arr, subsetSum, n, dp);
     }
 };
 
