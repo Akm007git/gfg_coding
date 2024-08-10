f716f8c17d3e018bb77c0c232a7ca392225b0ea1
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2024-08-10 15:40:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void  solve(stack<int>&st,int ele)
+{
+    //base
+    if(st.empty() ||  st.top() <= ele)
+    {
+        st.push(ele);
+        return;
+    }
+    
+    // others
+    int num = st.top();
+    st.pop();
+    
+    solve(st,ele);
+    st.push(num);
+}
+void SortedStack :: sort()
+{
+   //Your code here
+   if(s.empty())
+   {
+       return;
+   }
+   
+   int top = s.top();
+   s.pop();
+   
+   sort();
+   return solve(s,top); // to set it in his right position
+}
