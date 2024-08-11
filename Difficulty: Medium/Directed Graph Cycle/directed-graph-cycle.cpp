9d8358eb3f4256a2451cc7d28157df6b19ed1d83
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2024-08-11 14:05:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -5,49 +5,51 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
     private:
-    
-    bool dfs(int start,vector<int>adj[],int visited[],int recurStack[])
+    bool solve(int start,vector<int>adj[],vector<bool>&visited,vector<bool>&recurstack)
     {
-        // initally
-        visited[start] = 1;
-        recurStack[start] = 1;
+        // initially
+        visited[start] = true;
+        recurstack[start] = true;
         
+        // adjcent
         for(auto it:adj[start])
         {
             if(!visited[it])
             {
-                if(dfs(it,adj,visited,recurStack))
+                if(solve(it,adj,visited,recurstack))
                 {
                     return true;
                 }
             }
-            
-            // visited is true meas visited but has to be in the dame parh thats why i has to be in the ssme path
-            else if(recurStack[it])
+            else // it is visited then
             {
-                return true;
+                
+                if(recurstack[it]) // if yes means in same oath is is peeviosly visited
+                {
+                    return true;
+                }
             }
         }
         
-        // when path end then just check that its recurstack visited or not
-        recurStack[start] = false;
+        // while returning from thr sme path make recurdatck false
+        recurstack[start] = false;
         return false;
     }
   public:
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        int visited[V] = {0};
-        int recurStack[V] = {0};
-        
+        vector<bool>recurstack(V,false);
+         vector<bool>visited(V,false);
         for(int i = 0;i<V;i++)
         {
             if(!visited[i])
             {
-                if(dfs(i,adj,visited,recurStack))
+                if(solve(i,adj,visited,recurstack))
                 {
                     return true;
                 }
+                
             }
         }
         return false;
