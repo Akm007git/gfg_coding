0a96d66bf4028b4264e20b587d9ac680f23bda47
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2024-08-19 16:37:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,76 +1,79 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-class Solution
-{
-    
+class Solution {
     private:
-    int solve(int w,int wt[],int val[],int n,vector<vector<int>>&dp)
+    int solve(int n,int w,vector<int>&wt,vector<int>&val,vector<vector<int>>&dp)
     {
-        if( n == 0 || w == 0)
-       {
-           return 0;
-       }
-       
-       
-       //memo
-       if(dp[w-1][n-1] != -1)
-       {
-           return dp[w-1][n-1];
-       }
-       // choice d
-       if(wt[n-1] <= w) // taking
-       {
-           return dp[w-1][n-1] =  max( val[n-1] + solve(w-wt[n-1],wt,val,n-1,dp), solve(w,wt,val,n-1,dp)); 
-       }
-       else // not taking
-       {
-           return dp[w-1][n-1] =  solve(w,wt,val,n-1,dp);
-       }
+        if(n == 0 || w == 0) return 0;
+        if(dp[n-1][w-1] != -1) return dp[n-1][w-1];
+        
+        // choice
+        if(wt[n-1] <= w)
+        {
+            return dp[n-1][w-1] = max(val[n-1] + solve(n-1,w - wt[n-1],wt,val,dp),solve(n-1,w,wt,val,dp));
+        }
+        else
+        {
+            return dp[n-1][w-1] = solve(n-1,w,wt,val,dp);
+        }
     }
-    public:
-    //Function to return max value that can be put in knapsack of capacity W.
-    int knapSack(int W, int wt[], int val[], int n) 
-    { 
-       //base case
-       vector<vector<int>>dp(W+1,vector<int>(n+1,-1));
-       
-       return solve(W,wt,val,n,dp);
-       
+  public:
+    // Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int w, vector<int>& wt, vector<int>& val) {
+        // Your code here
+        
+        int n = wt.size();
+        
+        vector<vector<int>>dp(n,vector<int>(w,-1));
+        return  solve(n,w,wt,val,dp);
+        
     }
 };
 
 //{ Driver Code Starts.
 
-int main()
- {
-    //taking total testcases
+int main() {
+    // taking total testcases
     int t;
-    cin>>t;
-    while(t--)
-    {
-        //reading number of elements and weight
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        // reading number of elements and weight
         int n, w;
-        cin>>n>>w;
-        
-        int val[n];
-        int wt[n];
-        
-        //inserting the values
-        for(int i=0;i<n;i++)
-            cin>>val[i];
-        
-        //inserting the weights
-        for(int i=0;i<n;i++)
-            cin>>wt[i];
+        vector<int> arr, val, wt, drr;
+        string ip;
+        int number;
+        getline(cin, ip);
+        stringstream ss(ip);
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        getline(cin, ip);
+        ss.clear();
+        ss.str(ip);
+
+        while (ss >> number) {
+            val.push_back(number);
+        }
+
+        w = arr[0];
+        n = val.size();
+        getline(cin, ip);
+        ss.clear();
+        ss.str(ip);
+
+        while (ss >> number) {
+            wt.push_back(number);
+        }
         Solution ob;
-        //calling method knapSack()
-        cout<<ob.knapSack(w, wt, val, n)<<endl;
-        
+        cout << ob.knapSack(w, wt, val) << endl;
     }
-	return 0;
+    return 0;
 }
 // } Driver Code Ends
